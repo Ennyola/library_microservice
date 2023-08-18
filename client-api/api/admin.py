71f7a6778fa46a32fe9052a56641d77bab49b00b
Ennyola <medunoyeeni@gmail.com>
2023-08-18 22:06:31 +0100

    added a list_display view for books in admin site

--- client-api/api/admin.py
+++ client-api/api/admin.py
@@ -1,9 +1,13 @@
-
 from django.contrib import admin
 
-from .models import Book,LoanedBook,User
+from .models import Book, LoanedBook, User
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(LoanedBook)
 admin.site.register(User)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publisher", "category", "borrowed")
