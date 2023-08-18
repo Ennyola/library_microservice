84d5d3b8be9d24e9ffce8c9d71e54b950daff23f
Ennyola <medunoyeeni@gmail.com>
2023-08-18 22:08:42 +0100

    added a list display for user

--- client-api/api/admin.py
+++ client-api/api/admin.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from .models import Book, LoanedBook, User
 
 # Register your models here.
-admin.site.register(Book)
 admin.site.register(LoanedBook)
-admin.site.register(User)
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publisher", "category", "borrowed")
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "first_name", "last_name")
