9fa997bd83d1d48e104c7296344b2d5ff181b8df
Ennyola <medunoyeeni@gmail.com>
2023-08-20 12:19:53 +0100

    added a Loanedbook modeladmin

--- client-api/api/admin.py
+++ client-api/api/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from .models import Book, LoanedBook, User
 
 # Register your models here.
-admin.site.register(LoanedBook)
 
 
 @admin.register(Book)
@@ -14,3 +13,30 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "first_name", "last_name")
+
+
+@admin.register(LoanedBook)
+class LoanedBookAdmin(admin.ModelAdmin):
+    list_display = ("get_user", "get_book", "date_borrowed", "return_date")
+
+    def get_user(self, obj: LoanedBook) -> str:
+        """ Returns the email of the user loaning a book
+
+        Args:
+            obj (LoanedBook): An instance of LoanedBook
+
+        Returns:
+            str: The User email
+        """        
+        return obj.user.email
+
+    def get_book(self, obj: LoanedBook) -> str:
+        """ Returns the book title being loaned
+
+        Args:
+            obj (LoanedBook): An instance of LoanedBook
+
+        Returns:
+            str: The Loaned book title
+        """        
+        return obj.book.title
--- client-api/api/models.py
+++ client-api/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.query import QuerySet
 
 import requests
 
@@ -11,14 +12,14 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
 class ExcludeBorrowedBookManager(models.Manager):
     """A model manager to exclude borrowed books from the queryset"""
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         books = requests.get("http://adminservice:8000/api/books/").json()
         for book in books:
             if super().get_queryset().filter(id=book["id"]).exists():
@@ -54,7 +55,7 @@ class LoanedBook(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """_summary_
 
         Returns:
