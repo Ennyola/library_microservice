c83bab0da703c814b69d8c946d4696cd74215ba6
Ennyola <medunoyeeni@gmail.com>
2023-09-03 20:48:16 +0100

    updated the admin requirements.txt file

--- admin_api/api/tasks.py
+++ admin_api/api/tasks.py
@@ -1,9 +1,7 @@
 from celery import shared_task, current_app
 
-
 @shared_task(name="fetch_users")
 def fetch_users():
     # Get users from client_api 
-    users_result = current_app.send_task("get_users")
-    users = users_result.get()
-    return users
+    return current_app.send_task("get_users")
+   
